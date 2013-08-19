322a4b04875f496712bcefcedde40e22fb36ee0b
Sidd Tewari <stewari@peregrinegroup.com>
2013-08-19 10:14:39 -0400

    Initial commit after creation - skp test

---
+++ todos/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todos::Application.config.secret_key_base = '9b4d2bd3555cce5eb7172157bc42cf8022602f715c4c82ad374ffc7124435018b9e60f21a15fa3e78e9ef79357333104140148d7f09649046be05bcec3397db5'
