4b03aa628977b0fcf4322abebcb70537ffc26296
Sidd Tewari <stewari@peregrinegroup.com>
2013-08-19 14:50:54 -0400

    Green suite; and mistakenly some refactoring as well

--- todos/spec/features/user_creates_todo_spec.rb
+++ todos/spec/features/user_creates_todo_spec.rb
@@ -1,14 +1,18 @@
 require 'spec_helper'
 
-feature 'Guest creates todo' do
+feature 'User creates todo' do
   scenario 'creates a new todo' do
     create_todo_named 'Buy milk'
+
     # expect(page).to have_css('.todos li', text: 'Buy milk') REFACTOR
     expect(page).to display_todo_named('Buy milk')
   end
 
   def create_todo_named(name)
     visit root_path
+    click_on 'Sign in'
+    fill_in 'Email', with: 'me@example.com'
+    click_on 'Submit'
     click_on 'Add new todo'
     fill_in 'Name', with: name
     click_on 'Submit'
